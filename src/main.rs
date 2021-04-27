61497bb5eb6e71daada84099123af7a1148f1167
Andy Yu <andyyu2004@gmail.com>
2021-04-27 22:44:09 +1200

    more or less copy paste from blog for vga section

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buf = 0xb8000 as *mut u8;
-    for (i, &byte) in "Hello World!".as_bytes().iter().enumerate() {
-        unsafe {
-            *vga_buf.offset(i as isize * 2) = byte;
-            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("hello world 2");
     loop {}
 }
