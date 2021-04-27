f3276076ba11d66f88914910ed66a8573d632a87
Andy Yu <andyyu2004@gmail.com>
2021-04-27 22:03:12 +1200

    init

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+    for (i, &byte) in "Hello World!".as_bytes().iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop {}
+}
